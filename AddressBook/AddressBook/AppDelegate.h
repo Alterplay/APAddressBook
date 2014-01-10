8326ee01f8bdbadb3434fff25faee01e975dfdd6
Alexey Belkevich <belkevich.alexey@gmail.com>
2014-01-10 10:44:12 +0200

    Added empty project

---
+++ AddressBook/AddressBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AddressBook
+//
+//  Created by Alexey Belkevich on 1/10/14.
+//  Copyright (c) 2014 alterplay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AddressBook/AddressBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AddressBook
+//
+//  Created by Alexey Belkevich on 1/10/14.
+//  Copyright (c) 2014 alterplay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
