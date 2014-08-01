cbe71e3a8b1dc0b8829b318065c8e6a56adbe701
Alexey Belkevich <belkevich.alexey@gmail.com>
2014-08-01 13:27:14 +0300

    Added example app in Swift

---
+++ Example/Swift/AddressBook/TableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  TableViewCell.swift
+//  AddressBook
+//
+//  Created by Alexey Belkevich on 7/31/14.
+//  Copyright (c) 2014 alterplay. All rights reserved.
+//
+
+import Foundation
+
+class TableViewCell: DTTableViewCell {
+    
+    init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        super.init(style: UITableViewCellStyle.Subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    override func updateWithModel(model: AnyObject!) {
+        let contact = model as APContact
+        self.imageView.image = contact.thumbnail
+        self.textLabel.text = self.contactName(contact)
+        self.detailTextLabel.text = self.contactPhones(contact)
+    }
+    
+    func contactName(contact :APContact) -> String {
+        if contact.firstName != nil && contact.lastName != nil {
+            return "\(contact.firstName) \(contact.lastName)"
+        }
+        else if contact.firstName != nil || contact.lastName != nil {
+            return contact.firstName ? "\(contact.firstName)" : "\(contact.lastName)"
+        }
+        else {
+            return "Unnamed contact"
+        }
+    }
+    
+    func contactPhones(contact :APContact) -> String {
+        if let phones = contact.phones {
+            let array = phones as NSArray
+            return array.componentsJoinedByString(" ")
+        }
+        return "No phone"
+    }
+}
---
+++ Example/Swift/AddressBook/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  AddressBook
+//
+//  Created by Alexey Belkevich on 7/31/14.
+//  Copyright (c) 2014 alterplay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: DTTableViewController {
+    
+    @IBOutlet weak var activity: UIActivityIndicatorView!
+    
+    let addressBook = APAddressBook()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.registerCellClass(TableViewCell.self, forModelClass: APContact.self)
+        self.addressBook.fieldsMask = APContactField.Default | APContactField.Thumbnail
+        self.addressBook.sortDescriptors = [NSSortDescriptor(key: "firstName", ascending: true),
+            NSSortDescriptor(key: "lastName", ascending: true)]
+        self.addressBook.filterBlock = {(contact: APContact!) -> Bool in
+            return contact.phones.count > 0
+        }
+        self.addressBook.loadContacts(
+            { (contacts: [AnyObject]!, error: NSError!) in
+                self.activity.stopAnimating()
+                if contacts {
+                    self.memoryStorage().addItems(contacts)
+                }
+                else if error {
+                    let alert = UIAlertView(title: "Error", message: error.localizedDescription,
+                        delegate: nil, cancelButtonTitle: "OK")
+                    alert.show()
+                }
+            })
+    }
+
+}
+
