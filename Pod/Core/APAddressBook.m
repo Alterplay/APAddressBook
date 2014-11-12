c181416f1ebba0ed51505df5e6869b941a7945a7
Tony Million <tonymillion@gmail.com>
2014-11-12 21:40:53 +0000

    call ABAddressBookGetPersonWithRecordID on same queue

--- Pod/Core/APAddressBook.m
+++ Pod/Core/APAddressBook.m
@@ -166,15 +166,19 @@ void APAddressBookExternalChangeCallback(ABAddressBookRef __unused addressBookRe
 
 - (APContact*) getContactByRecordID:(NSNumber*)recordID
 {
-    // GET rule, we dont have to release `ref`
-    ABRecordRef ref = ABAddressBookGetPersonWithRecordID(self.addressBook, recordID.intValue);
+    __block APContact * newContact = nil;
+    dispatch_sync(self.localQueue, ^
+                  {
+                      // GET rule, we dont have to release `ref`
+                      ABRecordRef ref = ABAddressBookGetPersonWithRecordID(self.addressBook, recordID.intValue);
 
-    if (ref == NULL) {
-        return nil;
-    }
+                      if (ref != NULL) {
+                          newContact =  [[APContact alloc] initWithRecordRef:ref fieldMask:self.fieldsMask];
+                      }
 
-    return [[APContact alloc] initWithRecordRef:ref fieldMask:self.fieldsMask];
-}
+                  });
+
+    return newContact;}
 
 
 @end
