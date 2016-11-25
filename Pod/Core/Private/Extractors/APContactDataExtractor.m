e06316a9b3da2ab513ec382ea70beb450fa3467d
Alexey Belkevich <belkevich.alexey@gmail.com>
2016-11-25 19:47:18 +0200

    Fixed social profile url access crash

--- Pod/Core/Private/Extractors/APContactDataExtractor.m
+++ Pod/Core/Private/Extractors/APContactDataExtractor.m
@@ -112,7 +112,8 @@
         APSocialProfile *profile = [[APSocialProfile alloc] init];
         NSString *socialService = dictionary[(__bridge NSString *)kABPersonSocialProfileServiceKey];
         profile.socialNetwork = [APSocialServiceHelper socialNetworkTypeWithString:socialService];
-        profile.url = dictionary[(__bridge NSString *)kABPersonSocialProfileURLKey];
+        NSString *urlString = dictionary[(__bridge NSString *)kABPersonSocialProfileURLKey];
+        profile.url = [[NSURL alloc] initWithString:urlString];
         profile.username = dictionary[(__bridge NSString *)kABPersonSocialProfileUsernameKey];
         profile.userIdentifier = dictionary[(__bridge NSString *)kABPersonSocialProfileUserIdentifierKey];
         [profiles addObject:profile];
